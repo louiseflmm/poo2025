eb6052ff6bfab6e9b35db640c12e9bb74cdf523a
louiseflmm <louiseflammarion@gmail.com>
2025-05-11 16:05:10 -0300

    Update and rename Q7 MaterialBiblioteca.java to Q7 Aviao.java

---
+++ Q7 Aviao.java
@@ -0,0 +1,10 @@
+public class Aviao extends Veiculo {
+    public Aviao(int capacidade, double velocidadeMaxima) {
+        super(capacidade, velocidadeMaxima);
+    }
+
+    @Override
+    public String tipoDeCombustivel() {
+        return "Querosene de aviação";
+    }
+}
