abb3cb53733552a0457a2eb0ab2979cb747dd7b0
louiseflmm <louiseflammarion@gmail.com>
2025-05-11 16:49:03 -0300

    Create Q16 Ingrediente.java

---
+++ Q16 Ingrediente.java
@@ -0,0 +1,11 @@
+public abstract class Ingrediente {
+    protected String nome;
+    protected double quantidade;
+
+    public Ingrediente(String nome, double quantidade) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+    }
+
+    public abstract String informar();
+}
