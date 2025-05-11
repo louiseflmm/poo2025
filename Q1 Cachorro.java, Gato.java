c1cef8f0b3290efc86ff3d07917a4bfdcb4706a5
louiseflmm <louiseflammarion@gmail.com>
2025-05-11 15:39:37 -0300

    Create Q1 Cachorro.java, Gato.java

---
+++ Q1 Cachorro.java, Gato.java
@@ -0,0 +1,11 @@
+public class Cachorro extends Animal {
+    public void som() {
+        System.out.println("O cachorro faz: Au Au");
+    }
+}
+
+public class Gato extends Animal {
+    public void som() {
+        System.out.println("O gato faz: Miau");
+    }
+}
