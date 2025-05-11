2089895d923f0cc09434bdd4cafff19477a5b5be
louiseflmm <louiseflammarion@gmail.com>
2025-05-11 16:19:52 -0300

    Create Q10 Designer.java

---
+++ Q10 Designer.java
@@ -0,0 +1,10 @@
+public class Designer extends MembroEquipe {
+    public Designer(String nome, String projeto) {
+        super(nome, projeto);
+    }
+
+    @Override
+    public String trabalhar() {
+        return "Criando design para o projeto.";
+    }
+}
