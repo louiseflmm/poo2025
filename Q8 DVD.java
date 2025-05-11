e925bcd44f5dd1a318f39fec13a16c309ae0af8c
louiseflmm <louiseflammarion@gmail.com>
2025-05-11 16:10:32 -0300

    Create Q8 DVD.java

---
+++ Q8 DVD.java
@@ -0,0 +1,10 @@
+public class DVD extends MaterialBiblioteca {
+    public DVD(String titulo, int prazoDevolucao) {
+        super(titulo, prazoDevolucao);
+    }
+
+    @Override
+    public double calcularMulta(int diasAtraso) {
+        return 5.0;
+    }
+}
