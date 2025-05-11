0ff0d3f30c62ede7d880fc2cdc16948bcff84798
louiseflmm <louiseflammarion@gmail.com>
2025-05-11 17:02:53 -0300

    Create Q19 Tarefa.java

---
+++ Q19 Tarefa.java
@@ -0,0 +1,9 @@
+public abstract class Tarefa {
+    protected String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public abstract String realizarTarefa();
+}
