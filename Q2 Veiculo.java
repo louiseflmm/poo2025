b7b8a319245e4dba3863499b2f91043206381a77
louiseflmm <louiseflammarion@gmail.com>
2025-05-11 15:41:35 -0300

    Create Q2 Veiculo.java

---
+++ Q2 Veiculo.java
@@ -0,0 +1,13 @@
+public abstract class Veiculo {
+    protected String marca;
+    protected String modelo;
+    protected int ano;
+
+    public Veiculo(String marca, String modelo, int ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+
+    public abstract void informacoes();
+}
