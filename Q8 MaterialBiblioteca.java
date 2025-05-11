304f59a890e952671a8f79d219c78643e11eb4ff
louiseflmm <louiseflammarion@gmail.com>
2025-05-11 16:08:59 -0300

    Create Q8 MaterialBiblioteca.java

---
+++ Q8 MaterialBiblioteca.java
@@ -0,0 +1,19 @@
+public abstract class MaterialBiblioteca {
+    protected String titulo;
+    protected int prazoDevolucao;
+
+    public MaterialBiblioteca(String titulo, int prazoDevolucao) {
+        this.titulo = titulo;
+        this.prazoDevolucao = prazoDevolucao;
+    }
+
+    public abstract double calcularMulta(int diasAtraso);
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public int getPrazoDevolucao() {
+        return prazoDevolucao;
+    }
+}
