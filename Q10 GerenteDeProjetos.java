affad00568428d5f65acf1e0d45d6217286c2265
louiseflmm <louiseflammarion@gmail.com>
2025-05-11 16:20:20 -0300

    Create Q10 GerenteDeProjetos.java

---
+++ Q10 GerenteDeProjetos.java
@@ -0,0 +1,10 @@
+public class GerenteDeProjetos extends MembroEquipe {
+    public GerenteDeProjetos(String nome, String projeto) {
+        super(nome, projeto);
+    }
+
+    @Override
+    public String trabalhar() {
+        return "Coordenando o progresso do projeto.";
+    }
+}
